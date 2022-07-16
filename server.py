ccd53136c5826513a8aa33af5c2d6fed93cbb8e1
Jackson Rudd <jarudd@microsoft.com>
2022-07-16 15:04:50 -0700

    done

--- server.py
+++ server.py
@@ -8,8 +8,12 @@ root:Node = get_root_from_pickle()
 
 
 def get_next_nodes(search_str):
-    return [{'name': child.name, 'num_of_sections':len(child.get_all_songs())} for child in root.search(search_str.replace(" ", "").split(',')).get_all_children_nodes()]
+    if search_str == "":
+        node = root
+    else: 
+        node = root.search(search_str.replace(" ", "").split(','))
+    return [{'name': child.name, 'num_of_sections':len(child.get_all_songs())} for child in node.get_all_children_nodes()]
 
 def get_songs(search_str, page_num, page_size):
-    return root.search(search_str.replace(" ", "").split(',')).get_all_songs()[page_num*page_size: (page_num+1)*page_size ]
+    return list(root.search(search_str.replace(" ", "").split(',')).get_all_songs())[page_num*page_size: (page_num+1)*page_size ]
 
